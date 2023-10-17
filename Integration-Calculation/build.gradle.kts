0f1e890b3d97cca4fed1fd44ddc4cebb6c4466d1
Mikhail <93517239+sckwokyboom@users.noreply.github.com>
2023-10-17 17:09:20 +0700

    Updated build-script.

--- Integration-Calculation/build.gradle.kts
+++ Integration-Calculation/build.gradle.kts
@@ -1,5 +1,8 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+
 plugins {
     kotlin("jvm") version "1.9.0"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
     application
 }
 
@@ -24,4 +27,20 @@ kotlin {
 
 application {
     mainClass.set("MainKt")
+}
+
+tasks {
+    named<ShadowJar>("shadowJar") {
+        from(project.sourceSets.main.get().output)
+        configurations = project.configurations.filter { it.name == "runtimeClasspath" }
+        archiveFileName.set("IntegrationCalculation.jar")
+        destinationDirectory.set(file("./out/artifacts"))
+        duplicatesStrategy = DuplicatesStrategy.INCLUDE
+    }
+}
+
+tasks {
+    build {
+        dependsOn(shadowJar)
+    }
 }
